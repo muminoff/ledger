f5a3a5c009662470f8b1b2d7863cb988081fd34b
Sardor Muminov <sardor@insilicogen.com>
2017-01-03 16:31:58 +0900

    20170103

--- alias.sh
+++ alias.sh
@@ -1,3 +1,3 @@
 alias l="ledger -f ledger.ledger"
-alias le="gpg -e -u sardor -r sardor -a ledger.ledger"
+alias le="gpg --batch --yes -e -u sardor -r sardor -a ledger.ledger"
 alias ld="gpg ledger.ledger.asc"
