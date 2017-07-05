a3e9f3a1f331784e1c420772700e71a423a26443
Sardor Muminov <sardor@insilicogen.com>
2017-07-05 17:52:24 +0900

    Changes 1499244742

--- alias.sh
+++ alias.sh
@@ -1,4 +1,4 @@
 alias l="ledger -f /Users/sardor/ledger/my.ldg"
 alias le="gpg --batch --yes -e -u sardor -r sardor -a /Users/sardor/ledger/my.ldg"
 alias ld="gpg /Users/sardor/ledger/my.ldg.asc"
-alias lp="cd /Users/sardor/ledger && git add . && git commit -m \"Changes `date +%s`\" && git push origin master"
+alias lp="le && cd /Users/sardor/ledger && git add . && git commit -m \"Changes `date +%s`\" && git push origin master"
